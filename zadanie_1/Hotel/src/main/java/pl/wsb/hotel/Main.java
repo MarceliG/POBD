edab3ed562711bfcf0885398326bace097250508
warzywko <adammen201300@gmail.com>
2024-04-05 19:41:45 +0200

    - Add 5 more properties to Room class.
    - Convert properties to private.
    - Add getters and setters.

--- zadanie_1/Hotel/src/main/java/pl/wsb/hotel/Main.java
+++ zadanie_1/Hotel/src/main/java/pl/wsb/hotel/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(client.getAllInformation());
         System.out.println("-----");
 
-        Room room = new Room("1", 8.0, 1, false);
+        Room room = new Room("1", 8.0, 1, false, 2, true, true, true, true);
         System.out.println("Room: ");
         System.out.println(room.getAllInformation());
         System.out.println("-----");
